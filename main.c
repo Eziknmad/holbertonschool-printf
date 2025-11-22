fb126a350e4cfd2402275c6cb5df16047a9a06e1
fabiola <fabiola@penguin>
2025-11-21 21:22:39 -0400

    cp the main files in to branch

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - tests the _printf implementation
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+int len1, len2;
+
+len1 = _printf("Char:[%c]\n", 'H');
+len2 = printf("Char:[%c]\n", 'H');
+_printf("Len1: [%d]\n", len1);
+printf("Len2: [%d]\n", len2);
+
+len1 = _printf("String:[%s]\n", "Hello, world!");
+len2 = printf("String:[%s]\n", "Hello, world!");
+_printf("Len1: [%d]\n", len1);
+printf("Len2: [%d]\n", len2);
+
+len1 = _printf("Percent:[%%]\n");
+len2 = printf("Percent:[%%]\n");
+_printf("Len1: [%d]\n", len1);
+printf("Len2: [%d]\n", len2);
+
+/* Unknown specifier demo */
+_printf("Unknown:[%r]\n");
+printf("Unknown:[%r]\n");
+
+/* NULL string check */
+_printf("Null string:[%s]\n", (char *)NULL);
+printf("Null string:[%s]\n", (char *)NULL);
+
+return (0);
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+int write_char(char c);
+int print_string(char *s);
+int print_char_arg(va_list ap);
+int print_number(int n);
+int handle_format(const char *format, int *i, va_list ap);
+
+#endif
